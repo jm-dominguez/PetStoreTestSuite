70dca7122cf3b0cd397f34b07b9416b4b2ccb838
sunday <jm.dominguez@uniandes.edu.co>
2020-03-23 15:24:35 -0500

    Pending code review

--- test/negative/order-negative.test.js
+++ test/negative/order-negative.test.js
@@ -7,17 +7,26 @@ describe('negative tests for the order services', () =>{
     let pet3;
     let order3;
     let invalidId = getAlphanumericValue(10);
+
+    let pet4;
+    let order4;
     //Setup
     beforeAll(async () => {  
         pet3 = fakePetData();
         order3 = fakeOrderData(pet3.id);
         order3.id = invalidId;
 
+        pet4 = fakePetData();
+        order4 = fakeOrderData(pet4.id);
+        order4.quantity = 500000000000000000000000000000000000000000000000000;
+
         await postPet(pet3);
+        await postPet(pet4);
     })
     //Teardown
     afterAll(async () => {
         await deletePet(pet3.id);
+        await deletePet(pet4.id)
     })
 
     test('postOrderWithInvalidId', () =>{
@@ -54,6 +63,16 @@ describe('negative tests for the order services', () =>{
             if(err.response !== undefined){
                 expect(err.response.status).toBe(400);
             }
+        })
+    });
+
+    test('createOrderWithInvalidQuantity', ()=>{
+        expect.assertions(1);
+        return postOrder(order4).then(res =>{
+            expect(res.status).not.toBe(200);
+        })
+        .catch(err =>{
+            expect(err.response.status).toBe(400);
         })
     })
 });
